87ad795b3725f8a897db379ccb7cbc2411d036c9
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-06-25 01:21:54 +0530

    🎉 Boiler Plate

---
+++ testing-react/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header";
+function App() {
+  return (
+    <div className="Container">
+    <h1>Text</h1>
+    <Header>
+
+    </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ testing-react/src/Components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+        <h1 style={colourscheme}>Admin Panel</h1>
+    </header>
+  )
+}
+
+const colourscheme = {
+    color: 'white',
+    backgroundColor: 'blue'
+
+}
+
+export default Header
