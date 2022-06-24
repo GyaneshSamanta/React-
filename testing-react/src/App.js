43c335295c832743dbfa5899abcb51dff345cd03
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-06-25 02:09:19 +0530

    💄 made buttons

--- testing-react/src/App.js
+++ testing-react/src/App.js
@@ -2,9 +2,7 @@ import Header from "./Components/Header";
 function App() {
   return (
     <div className="Container">
-    <h1>Text</h1>
     <Header>
-
     </Header>
     </div>
   );
--- testing-react/src/Components/Header.js
+++ testing-react/src/Components/Header.js
@@ -1,11 +1,17 @@
 const Header = () => {
   return (
     <header>
-        <h1 style={colourscheme}>Admin Panel</h1>
+        <h1 style={colourscheme}>Admin Panel</h1><br></br>
+        <button style={buttonstyle}>Sign Up</button> <br></br>
+        <button style={buttonstyle}>Log In</button>
+
     </header>
   )
 }
+const buttonstyle = {
+  backgroundcolor: 'white',
 
+}
 const colourscheme = {
     color: 'white',
     backgroundColor: 'blue'
